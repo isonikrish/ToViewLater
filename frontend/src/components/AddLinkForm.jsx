8df5ef51a5266973f03ec30da5f7b9044bced6b5
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-09-27 15:08:46 +0100

    Add button next to input which sets input to current tab url.

--- frontend/src/components/AddLinkForm.jsx
+++ frontend/src/components/AddLinkForm.jsx
@@ -2,9 +2,10 @@
 
 import React, { useState, useContext } from 'react';
 import { LinkContext } from '../context/LinkContext';
+import { TbBrowserPlus } from "react-icons/tb";
 
 const AddLinkForm = () => {
-    const { addLink } = useContext(LinkContext);
+    const { addLink, getCurrentUrl } = useContext(LinkContext);
     const [link, setLink] = useState('');
     const [note, setNote] = useState('');
 
@@ -18,14 +19,27 @@ const AddLinkForm = () => {
     return (
         <form onSubmit={handleSubmit} className="bg-white p-4 rounded shadow-md">
             <h2 className="text-lg font-semibold mb-2">Add Link</h2>
+            <div className="w-full flex justify-between h-12 gap-2 py-2">
             <input
                 type="text"
                 value={link}
                 onChange={(e) => setLink(e.target.value)}
                 placeholder="Enter link"
-                className="border border-gray-300 p-2 mb-2 w-full rounded"
+                className="border border-gray-300 p-2 h-full rounded flex-grow"
                 required
             />
+            <button 
+                type="button" 
+                className="bg-blue-500 text-white p-2  h-full rounded hover:bg-blue-600"
+                onClick={async () => {
+                    const currentUrl = await getCurrentUrl()
+                    setLink(currentUrl)}
+                }
+                >
+            <TbBrowserPlus />
+            </button>
+            </div>
+            
             <textarea
                 value={note}
                 onChange={(e) => setNote(e.target.value)}
